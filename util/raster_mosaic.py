bde5d40925520fc1c420421ec8873db0291baf71
OlgaWold <oboyantseva.1996@gmail.com>
2021-04-07 22:29:38 +0300

    1) In configuration file added overlapping argument 0.0625 (32 pix) 2) In raster_mosaic.py added new function that takes overlap into account 3) cm_predict.py added new function there, where "crop" argument is 1/2 of overlap from config file. 4) Commented predict and sub-tile function in cm_predict.py

--- util/raster_mosaic.py
+++ util/raster_mosaic.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import glob
 import os
-from PIL import Image
+from PIL import Image, ImageOps
 from math import ceil, floor
 import re
 import numpy as np
@@ -53,4 +53,80 @@ def image_grid(image_list, rows, cols):
         new_img.paste(img, box=(i % cols * w, i // cols * h))
     return new_img
 
+
+def image_grid_overlap(image_list, rows, cols, crop):
+    w, h = Image.open(image_list[0]).size
+    new_img = Image.new('RGB', size=(cols * (w - crop * 2), rows * (h - crop * 2)))
+    grid_w, grid_h = new_img.size
+
+    off_x = 0
+    off_y = 0
+
+    # Creates new empty image with taking the size from sub-tile from the list
+
+    for i, img in enumerate(image_list):
+        img = Image.open(img)
+
+        border = (crop, crop, crop, crop)  # left, up, right, bottom
+
+        # Get row and column number
+        col_id = i % cols
+        row_id = i // cols
+
+        # Set conditions to crop images differently.
+        # Corners should be cropped first, then the borders
+
+        if (row_id == 0 and col_id == 0):
+            img_crop = ImageOps.crop(img, (0, 0, crop, crop))  # upper left corner
+
+        elif (i % cols == 0 and row_id == (rows - 1)):
+            img_crop = ImageOps.crop(img, (0, crop, crop, 0))  # lower left corner
+
+        elif (row_id == 0 and col_id == (cols - 1)):
+            img_crop = ImageOps.crop(img, (crop, 0, 0, crop))  # upper right corner
+
+        elif (row_id == (rows - 1) and col_id == (cols - 1)):
+            img_crop = ImageOps.crop(img, (crop, crop, 0, 0))  # lower right corner
+
+        elif row_id == 0:
+            img_crop = ImageOps.crop(img, (crop, 0, crop, crop))  # 1st row
+
+        elif (col_id == 0):
+            img_crop = ImageOps.crop(img, (0, crop, crop, crop))  # 1st column
+
+        elif row_id == (rows - 1):
+            img_crop = ImageOps.crop(img, (crop, crop, crop, 0))  # last row
+
+        elif col_id == (cols - 1):
+            img_crop = ImageOps.crop(img, (crop, crop, 0, crop))  # last column
+
+        else:
+            img_crop = ImageOps.crop(img, border)
+
+        # When all images in the list cropped, paste them in the new empty image
+
+        box = (off_x, off_y)
+        new_img.paste(img_crop, box=box)
+
+        '''
+        For the first tile set the offset 0,0
+        As the tiles are pasted row by row, every time it will go to first row x offset is reset to 0
+
+        As most of tiles are cropped 16 pixels from all 4 sides the offset should be shifted crop*2 on X and Y axis
+        Border tiles are cropped differently, so the offsets are shifted only for "crop" argument
+        '''
+        if col_id == 0:
+            off_x += w - crop
+        elif col_id == cols - 1:
+            off_x = 0
+        else:
+            off_x += w - crop * 2
+
+        if col_id == cols - 1:
+            if row_id == 0:
+                off_y += h - crop
+            else:
+                off_y += h - crop * 2
+
+    return new_img
 
