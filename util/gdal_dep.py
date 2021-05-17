e8fdb911270363d3e81d87aee2e608de5af6aa58
OlgaWold <oboyantseva.1996@gmail.com>
2021-05-17 14:47:39 +0300

    Added new function with gdal dependencies (where all the work with projection is going)

---
+++ util/gdal_dep.py
@@ -0,0 +1,31 @@
+import gdal
+import os
+
+'''
+1) Open any .jp2 file from initial product (10m band), transform it into GeoTiff
+2) Get projection
+3) Apply it for the final prediction mosaic in .tif format 
+'''
+
+def get_projection (image_list, big_im_path, tif_mosaic):
+    in_image = gdal.Open(image_list[0])
+    driver = gdal.GetDriverByName("GTiff")
+    out_image = driver.CreateCopy((big_im_path + "/" + 'projection.tif'), in_image, 0)
+
+    in_image = None
+    out_image = None
+
+    tif = gdal.Open(big_im_path + "/" + 'projection.tif')
+
+
+    prj = tif.GetProjection()
+    gt = tif.GetGeoTransform()
+
+    mosaic = gdal.OpenShared(tif_mosaic, gdal.GA_Update)
+    mosaic.SetProjection(prj)
+    mosaic.SetGeoTransform(gt)
+
+    # Delete the Geotiff projection/transformation file
+    os.remove(big_im_path + "/" + 'projection.tif')
+
+    return mosaic
