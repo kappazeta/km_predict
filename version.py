1b92185111435459b23cb26413afc5fa7cd15f15
Anton <toshaklg@gmail.com>
2021-06-29 13:30:25 +0300

    Geo-reference fix for L1C product, minor changes

--- version.py
+++ version.py
@@ -1,3 +1,4 @@
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
+# 1.0.1 - L1C support, new weights files
 # 1.0.0 - cm_predict version implementation, logger implementation, image rotating  on re-creation fix
