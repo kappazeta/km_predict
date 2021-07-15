b74fbcab05701e95760141e83c5a79efb017c9b8
Anton <toshaklg@gmail.com>
2021-07-15 08:52:41 +0300

    Missing variable

--- cm_predict.py
+++ cm_predict.py
@@ -230,7 +230,7 @@ class CMPredict(ulog.Loggable):
         2) Sets final image size from col*width, rows*height
         3) Creates final image from all sub-tiles, bounding box parameters are also set 
         """
-        
+        overlap_pix = self.overlapping * self.tile_size
         crop_coef = int(overlap_pix / 2)
         n_rows = math.ceil(10980 / (self.tile_size - crop_coef))
         new_im = image_grid_overlap(image_list, rows=n_rows, cols=n_rows, crop=crop_coef)
